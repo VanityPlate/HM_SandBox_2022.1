8a992ba7bf066c0e6729fa928b8e57c1a95a584b
alex.ducken <alexducken@gmail.com>
2022-04-20 12:11:34 -0700

    --Uploading to sandbox code base for testing updates to surcharges

---
+++ src/FileCabinet/SuiteScripts/Invoice/MS_Library.js
@@ -0,0 +1,127 @@
+/**
+ *
+ * @copyright Alex S. Ducken 2021 alexducken@hydramaster.com
+ * HydraMaster LLC
+ *
+ * Updated 1/5/2022 Price changes and percentiles added for classes. Surcharge no longer related to dates.
+ * Update 3/28/2022 Included DATEREF to change the business logic to account for dates of creation on sales orders
+ * Update 3/18/2022 Adding itemsDateDependent refactoring surcharging to use fields in NetSuite instead of this library
+ *                  so removing unneeded references
+ *
+ * @NApiVersion 2.1
+ */
+define([],
+    
+    () => {
+
+        //Updated 3/28/2022
+        const DATEREF = new Date.parse(new Date(2022, 1, 1));
+        //Items in this array are dependent on the above date
+        const itemsDateDependent = [15223, 15220, 15225, 15221, 15224, 15222];
+
+        //Deprecated 4/18/2022
+        const chargesByClass = {
+            19: {surchargePercent:  .1 ,surchargeFlat: -1}, //Chemicals
+            78: {surchargePercent: .08, surchargeFlat: -1}, //Parts
+            3: {surchargePercent: .1, surchargeFlat: -1}, //Accessories HM Tools
+            4: {surchargePercent: .1, surchargeFlat: -1}, //DRIMASTER
+            5: {surchargePercent: .1, surchargeFlat: -1}, //EVOLUTION WAND
+            6: {surchargePercent: .1, surchargeFlat: -1}, //OTHER TOOL ACCESSORIES
+            8: {surchargePercent: .1, surchargeFlat: 452.00}, //RX-15
+            9: {surchargePercent: .1, surchargeFlat: 452.00}, //RX-20
+            10: {surchargePercent: .1, surchargeFlat: -1}, //STEEL WANDS
+            11: {surchargePercent: .1, surchargeFlat: -1}, //Accessories Truckmount
+            12: {surchargePercent: .1, surchargeFlat: -1}, //APO
+            13: {surchargePercent: .1, surchargeFlat: -1}, //CRADLE TANK
+            14: {surchargePercent: .1, surchargeFlat: -1}, //FRESH WATER TANK
+            15: {surchargePercent: .1, surchargeFlat: -1}, //OTHER TM ACCESSORIES
+            16: {surchargePercent: .1, surchargeFlat: -1}, //REELS
+            17: {surchargePercent: .1, surchargeFlat: -1}, //SHELVES
+            18: {surchargePercent: .1, surchargeFlat: -1}, //VACUUM/SOLUTION HOSE
+            71: {surchargePercent: .0, surchargeFlat: 1140.00}, //Titan 325
+            72: {surchargePercent: 0, surchargeFlat: 1228.80}, //Titan 425
+            73: {surchargePercent: 0, surchargeFlat: 1610.10}, //Titan 575
+            114: {surchargePercent: 0, surchargeFlat: 1797.45}, //Titan 625
+            124: {surchargePercent: 0, surchargeFlat: 1378.42}, //CDS 4.8 SV
+            76: {surchargePercent: 0, surchargeFlat: 1560.00}, //TMTG 4000
+            75: {surchargePercent: 0, surchargeFlat: 1476.00}, //TITAN H2O
+            22: {surchargePercent: 0, surchargeFlat: 1228.42}, //CDS 4.8
+            24: {surchargePercent: 0, surchargeFlat: 1050.00}, //CDS XDRIVE
+            34: {surchargePercent: 0, surchargeFlat: 882.70}, //PEX 500
+            23: {surchargePercent: 0, surchargeFlat: 1378.42}, //CDS FLEX
+            115: {surchargePercent: 0, surchargeFlat: 1378.42}, //CDS HIACE
+            26: {surchargePercent: 0, surchargeFlat: 1378.42}, //CDS ZER-REZ
+            52: {surchargePercent: 0, surchargeFlat: 1140.00}, //Boxxer 318
+            63: {surchargePercent: 0, surchargeFlat: 1140.00}, //DIAMOND
+            58: {surchargePercent: 0, surchargeFlat: 1228.80}, //Boxxer XL
+            55: {surchargePercent: 0, surchargeFlat: 1228.80}, //Boxxer 423
+            64: {surchargePercent: 0, surchargeFlat: 1228.80}, //DIAMOND
+            112: {surchargePercent: 0, surchargeFlat: 1228.80}, //CTS 403
+
+        };
+
+        //Deprecated 4/18/2022
+        const chargesById = {
+            3003: {surchargePercent: 0, defaultCharge: 330.00}, //000-047-028 18hp
+            15957: {surchargePercent: 0, defaultCharge: 330.00}, //000-047-044 18hp modified
+            3005: {surchargePercent: 0, defaultCharge: 475.00}, //000-047-037 23hp
+            15956: {surchargePercent: 0, defaultCharge: 475.00}, //000-047-043 23hp modified
+            3006: {surchargePercent: 0, defaultCharge: 500.00}, //000-047-040 31hp
+            //Below Added on 1/5/2022 As Part of New Surcharge
+            15546: {surchargePercent: 0, defaultCharge: 1140.00}, //750-012-751-10
+            15541: {surchargePercent: 0, defaultCharge: 1140.00}, //750-011-751-10
+            15547: {surchargePercent: 0, defaultCharge: 1228.80}, //750-012-752-10
+            15542: {surchargePercent: 0, defaultCharge: 1228.80}, //750-011-752-10
+            15548: {surchargePercent: 0, defaultCharge: 1610.40}, //750-012-753-10
+            15543: {surchargePercent: 0, defaultCharge: 1610.40}, //750-011-753-10
+            26871: {surchargePercent: 0, defaultCharge: 1797.45}, //750-012-759-10
+            26872: {surchargePercent: 0, defaultCharge: 1797.45}, //750-011-759-10
+            15552: {surchargePercent: 0, defaultCharge: 1560.00}, //750-013-756-10
+            15522: {surchargePercent: 0, defaultCharge: 1476.00}, //750-010-743-10
+            15569: {surchargePercent: 0, defaultCharge: 1228.42}, //751-011-701-10
+            26881: {surchargePercent: 0, defaultCharge: 1378.42}, //751-011-744-10
+            15610: {surchargePercent: 0, defaultCharge: 1050.00}, //751-024-705-10
+            15407: {surchargePercent: 0, defaultCharge: 452.00}, //700-041-030
+            15435: {surchargePercent: 0, defaultCharge: 452.00}, //700-041-333
+            15436: {surchargePercent: 0, defaultCharge: 452.00}, //700-041-334
+            15410: {surchargePercent: 0, defaultCharge: 452.00}, //700-041-035
+            15237: {surchargePercent: 0, defaultCharge: 560.63}, //56384887
+            15238: {surchargePercent: 0, defaultCharge: 882.70}, //56385431
+            //Tread Master Pricing added 1/10/2022
+            15223: {surchargePercent: 0.07, defaultCharge: -1, datePrior: DATEREF.newYear}, //100-100-004
+            15220: {surchargePercent: 0.07, defaultCharge: -1, datePrior: DATEREF.newYear}, //100-100-001
+            15225: {surchargePercent: 0.07, defaultCharge: -1, datePrior: DATEREF.newYear}, //100-100-006
+            15221: {surchargePercent: 0.07, defaultCharge: -1, datePrior: DATEREF.newYear}, //100-100-002
+            15224: {surchargePercent: 0.07, defaultCharge: -1, datePrior: DATEREF.newYear}, //100-100-005
+            15222: {surchargePercent: 0.07, defaultCharge: -1, datePrior: DATEREF.newYear}, //100-100-003
+            1960: {surchargePercent: 0.07, defaultCharge: -1}, //000-031-105
+            17742: {surchargePercent: 0.07, defaultCharge: -1}, //190-100-010
+            17743: {surchargePercent: 0.07, defaultCharge: -1}, //190-100-011
+            17748: {surchargePercent: 0.07, defaultCharge: -1}, //190-100-016
+            17744: {surchargePercent: 0.07, defaultCharge: -1}, //190-100-012
+            17747: {surchargePercent: 0.07, defaultCharge: -1}, //190-100-015
+            17745: {surchargePercent: 0.07, defaultCharge: -1}, //190-100-013
+            1744: {surchargePercent: 0.07, defaultCharge: -1}, //000-016-060
+            3082: {surchargePercent: 0.07, defaultCharge: -1}, //000-049-115
+            16558: {surchargePercent: 0.07, defaultCharge: -1}, //000-078-201
+            16560: {surchargePercent: 0.07, defaultCharge: -1}, //000-078-203
+            16561: {surchargePercent: 0.07, defaultCharge: -1}, //000-078-205
+            17513: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-040
+            17517: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-044
+            17512: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-039
+            17518: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-045
+            17511: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-038
+            17519: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-046
+            17508: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-034
+            17514: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-041
+            17509: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-036
+            17515: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-042
+            17510: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-037
+            17516: {surchargePercent: 0.07, defaultCharge: -1}, //101-100-043
+        };
+        
+        const surchargeItem = 27666;
+
+        return {DATEREF, surchargeItem, itemsDateDependent}
+
+    });
---
+++ src/FileCabinet/SuiteScripts/Invoice/Machine_Surcharge.js
@@ -0,0 +1,134 @@
+/**
+ *
+ * @copyright Alex S. Ducken 2021 alexducken@hydramaster.com
+ * HydraMaster LLC
+ *
+ * Update 1/5/2022 Dates no longer considered, consider on per item basis first then classes, allow for percentage increase
+ * Update 3/28/2022 Including logic to consider dates held by MS_Library.js
+ * Update 4/18/2022 Refactoring to use NetSuite fields instead of library making changes to how items that are dependent
+ *                  on dates are checked but not to the business logic
+ *
+ * @NApiVersion 2.1
+ * @NScriptType WorkflowActionScript
+ */
+define(['N/search', 'N/record', './MS_Library', 'N/currentRecord'],
+    
+    (search, record, msLib, currentRecord) => {
+        /**
+         * Defines the WorkflowAction script trigger point.
+         * @param {Object} scriptContext
+         * @param {Record} scriptContext.newRecord - New record
+         * @param {Record} scriptContext.oldRecord - Old record
+         * @param {string} scriptContext.workflowId - Internal ID of workflow which triggered this action
+         * @param {string} scriptContext.type - Event type
+         * @param {Form} scriptContext.form - Current form that the script uses to interact with the record
+         * @since 2016.1
+         */
+        const onAction = (scriptContext) => {
+
+            try{
+                let recordObj = scriptContext.newRecord;
+                let surcharge = 0.0;
+                let lines = recordObj.getLineCount({sublistId: 'item'});
+                for(let x = 0; x < lines; x++){
+                    let itemClass = null;
+                    let itemId = '';
+                    let itemDollarSur = null;
+                    let itemSur = null;
+                    let classDollarSur = null;
+                    let classSur = null;
+                    itemId = recordObj.getSublistValue({sublistId: 'item', fieldId: 'item', line: x});
+                    //Refactor Building Out Changes
+                    let itemFields = search.lookupFields({
+                        type: search.Type.ITEM,
+                        id: itemId,
+                        columns: ['custitem_item_surcharge', 'custitem_item_surcharge_dollar']
+                    });
+                    //Refactor Testing
+                    log.audit({title: 'itemFields', details: itemFields});
+                    itemDollarSur = itemFields.custitem_item_surcharge[0].value;
+                    itemSur = itemFields.custitem_item_surcharge_dollar[0].value;
+                    try{
+                        itemClass = search.lookupFields({
+                            type: search.Type.ITEM,
+                            id: itemId,
+                            columns: ['class']
+                        }).class[0].value;
+                    }
+                    catch (e) {
+                        //do nothing
+                    }
+                    if(itemClass != null){
+                        let classResults = search.lookupFields({
+                            type: search.Type.CLASSIFICATION,
+                            id: itemClass,
+                            columns: ['custrecord_class_surcharge_dollar_value', 'custrecord_class_surcharge']
+                        });
+                        //Refactor Testing
+                        log.audit({title: 'classResults', details: classResults});
+                        classDollarSur = classResults.custrecord_class_surcharge_dollar_value[0].value;
+                        classSur = classResults.custrecord_class_surcharge[0].value;
+                    }
+                    //Below before initial record save returns original date created
+                    let dateCreated = Date.parse(recordObj.getValue({fieldId: 'createddate'}).toDateString());
+                    let dateDependent = new Set(msLib.itemsDateDependent);
+                    dateDependent = dateDependent.has(itemId);
+                    //Making sure dates are not prior to set date if
+                    if(dateDependent == false || dateCreated >= msLib.DATEREF) {          //Added 3/28/2022 Checking dates
+                        //Commented Out Saved for Testing
+                        //log.audit({title: 'Item Class', details: itemClass});
+                        //Working on per item basis
+                        //Working with percent increase on items
+                        if (itemSur != null && itemSur > 0) {
+                            let total = recordObj.getSublistValue({sublistId: 'item', fieldId: 'amount', line: x});
+                            //Commented Out Saved for Testing
+                            //log.audit({title: 'test total id', details: total});
+                            surcharge += itemSur * total;
+                        }
+                        //Working with flat rate on items
+                        else if(itemDollarSur != null && itemDollarSur > 0){
+                            let quantity = recordObj.getSublistValue({
+                                sublistId: 'item',
+                                fieldId: 'quantity',
+                                line: x
+                            });
+                            surcharge += itemDollarSur * quantity;
+                        }
+                        //Working on per item_class basis
+                        //Working with percent increase on items
+                        else if (classSur != null && classSur > 0) {
+                            let total = recordObj.getSublistValue({sublistId: 'item', fieldId: 'amount', line: x});
+                            //Commented Out Saved for Testing
+                            //log.audit({title: 'test total class', details: total});
+                            surcharge += classSur * total;
+                        }
+                        //Working with flat rate on items
+                        else if (classDollarSur != null && classDollarSur > 0){
+                            let quantity = recordObj.getSublistValue({
+                                sublistId: 'item',
+                                fieldId: 'quantity',
+                                line: x
+                            });
+                            surcharge += classDollarSur * quantity;
+                        }
+                        //Commented Out Saved for Testing
+                        //log.audit({title: 'Running Surcharge Total', details: surcharge});
+                    }
+                }
+                if(surcharge > 0){
+                    let discount = recordObj.getValue({fieldId: 'discounttotal'});
+                    recordObj.selectNewLine({sublistId: 'item'})
+                    recordObj.setCurrentSublistValue({sublistId: 'item', fieldId: 'quantity', value: 1});
+                    recordObj.setCurrentSublistValue({sublistId: 'item', fieldId: 'item', value: msLib.surchargeItem});
+                    recordObj.setCurrentSublistValue({sublistId: 'item', fieldId: 'amount', value: surcharge});
+                    recordObj.commitLine({sublistId: 'item'});
+                    recordObj.setValue({fieldId: 'discountrate', value: discount});
+                }
+            }
+            catch (e) {
+                log.error({title: 'Critical Error in onAction', details: e});
+            }
+        }
+
+        return {onAction};
+    });
